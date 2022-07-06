347da4973d34a059267fb353bed048002111c0fb
Nguyet Ha Phung <Nguyet-Ha1.Phung@stud.uni-regensburg.de>
2022-07-06 11:47:17 +0200

    Update Word.java

--- src/Word.java
+++ src/Word.java
@@ -3,8 +3,41 @@ import lingologs.Script;
 public class Word {
 
     private Script content;
+    private boolean isComposition;
+    private boolean isAbbreviations;
+    private boolean isForeign;
+    private boolean isPassive;
 
     public Word(Script content) {
         this.content = content;
+        this.isComposition = false;
+        this.isAbbreviations = false;
+        this.isForeign = false;
+        this.isPassive = false;
+    }
+
+
+    public Script getContent() {
+        return content;
+    }
+
+    public int getLength(){
+        return content.length();
+    }
+
+    public boolean isComposition() {
+        return isComposition;
+    }
+
+    public boolean isAbbreviations() {
+        return isAbbreviations;
+    }
+
+    public boolean isForeign() {
+        return isForeign;
+    }
+
+    public boolean isPassive() {
+        return isPassive;
     }
 }
