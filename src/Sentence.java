4036657ff9659d809e7176bb46e1d38edb3d1e05
Nguyet Ha Phung <Nguyet-Ha1.Phung@stud.uni-regensburg.de>
2022-07-06 13:22:42 +0200

    update Sentence

--- src/Sentence.java
+++ src/Sentence.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Sentence {
     private Script content;
     private Script normalizedContent;
-    private List<Word> words;
+    private ArrayList<Word> words;
     private int numCommas;
 
     public Sentence(Script content) {
@@ -15,16 +15,28 @@ public class Sentence {
         this.content = content;
         //this.normalizedContent = sentenceProcessor.normalize()
         //this.words = splitSentenceIntoWords(content);
-        int numCommas = 0;
+        numCommas = 0;
+        words = new ArrayList<>();
     }
 
-    public List<Script> splitSentenceIntoWords() {
-        List<Script> sentencesList = content.split("[!?.:]+");
-        return sentencesList;
+    public ArrayList<Word> splitSentenceIntoWords() {
+        List<Script> wordsList = content.split("\\w+");
+        for (Script word : wordsList){
+            words.add(new Word(word));
+        }
+        return words;
     }
 
     public int getNumCommas(){
         numCommas = content.count(",");
         return numCommas;
+    }
+
+    public Script getContent() {
+        return content;
+    }
+
+    public ArrayList<Word> getWords() {
+        return words;
     }
 }
