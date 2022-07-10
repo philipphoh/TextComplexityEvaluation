ff22450c6af359b697f4579ecc50a34cb83a5b23
Nguyet Ha Phung <Nguyet-Ha1.Phung@stud.uni-regensburg.de>
2022-07-10 22:55:32 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
+import lingologs.Script;
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class Main {
     private static String textAsString;
 
     public static void main(String[] args) throws IOException {
-        readFile();
-        System.out.printf(textAsString);
-        
+//        readFile();
+//        System.out.printf(textAsString);
+
+        Text text = new Text(new Script("hello hello abstrakt how are you abbauprodukt."));
+        Evaluator evaluator = new Evaluator(text);
+        System.out.println(evaluator.getEntropy());
+        System.out.println(evaluator.countForeignWords());
+        System.out.println(evaluator.getReadabilityScore());
+        System.out.println(evaluator.countCompoundWords());
+
     }
 
     private static void readFile() throws IOException {
@@ -22,107 +32,4 @@ public class Main {
         textAsString = Files.readString(Path.of(filePath), StandardCharsets.UTF_8);
     }
 
-    /*
-        BufferedReader br = new BufferedReader(fr);
-        String line;
-        while ((line = br.readLine()) != null){
-            foreignWordsList.add(line);
-            for (Script word : wordsList){
-                for (String foreignWord : foreignWordsList){
-                    if (word.toString().toLowerCase().equals(foreignWord.toLowerCase())){
-                        foreignWordsInText.add(word);
-                    }
-                }
-            }
-        }
-        return foreignWordsInText;
-    }
-
-    private static boolean checkAbbreviations(Script text) throws IOException {
-//        File abbreviationFile = new File("./src/Data/Abkürzungen.txt");
-//        FileReader fr = new FileReader(abbreviationFile);
-//
-//        ArrayList<String> abbreviationsList = new ArrayList<>();
-//        ArrayList<Script> abbreviationsInText = new ArrayList<>();
-//
-//        BufferedReader br = new BufferedReader(fr);
-//        String line;
-//        while ((line = br.readLine()) != null){
-//
-//        }
-        //funktioniert nur bei den Abkürzungen
-        boolean isAbbreviations = text.match("([A-ZÜÖÄßa-zäöü]*.( |-)?){1,}");
-
-        return isAbbreviations;
-    }
-
-    //Satzebene
-
-    private static void getMeanNumWordsPerSentence(Script text) {
-
-    }
-
-    private static double getAverageValueSentence(Script text, ArrayList<Integer> arr) {
-        List<Script> sentencesList = text.split("[!?.:]+");
-
-        for (Script sentence : sentencesList){
-            arr.add(getNumWords(sentence));
-        }
-
-        double meanValue = Mathx.mean(arr);
-        return meanValue;
-    }
-    //Wortebene
-
-    private static double getAverageLengthOfWord (Script text){
-        ArrayList<Script> wordsList = getWordList(text);
-        List<Integer> lengthWordList = new ArrayList<>();
-
-        for (Script word: wordsList){
-            lengthWordList.add(getLengthWord(word));
-        }
-
-        double meanLengthWord = Mathx.mean(lengthWordList);
-        return meanLengthWord;
-    }
-
-    /* Verarbeitung der Daten der Überprüfung */
-    //funktioniert noch nix:< Warummmmmmm
-
-    /*
-    private static double getEntropy (Script text){
-        return Mathx.info(getWordFrequency(text));
-    }
-
-    private static List<Integer> getWordFrequency (Script text){
-        HashMap<Script, Integer> wordCount = new HashMap<>();
-        ArrayList<Script> wordsList = getWordList(text);
-
-        for (Script word : wordsList){
-            Script processed = word.toLower();
-            if (wordCount.containsKey(processed)){
-                wordCount.put(processed, wordCount.get(processed) + 1);
-            }
-            else {
-                wordCount.put(processed, 1);
-            }
-        }
-        List<Integer> frequenciesList = new ArrayList<>(wordCount.values());
-        return frequenciesList;
-    }
-
-
-
-
-
-    private static ArrayList<Script> getWordList(Script text){
-        ArrayList<Script> words = new ArrayList<>();
-        List<Script> wordListFromText = text.split("\\w+");
-
-        for (Script word:wordListFromText){
-            words.add(word);
-        }
-        return words;
-    }
-    */
 }
